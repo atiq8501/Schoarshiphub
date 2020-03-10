f074afa8aa278a6059a5699635f233534b4c91d1
Atiqur Rahman Foyshal <atiqur.foyshal@gmail.com>
2020-03-11 00:11:20 +0600

    applications, offers releted operation done

---
+++ ScholarshipHub/Repository/ApplictionsToUniversityRepository.cs
@@ -0,0 +1,21 @@
+﻿using ScholarshipHub.Interfaces;
+using ScholarshipHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScholarshipHub.Repository
+{
+    public class ApplictionsToUniversityRepository : Repository<ApplictionsToUniversity>, IApplictionsToUniversityRepository
+    {
+        public IEnumerable<ApplictionsToUniversity> GetAll(int uniOfferId)
+        {
+            return context.Set<ApplictionsToUniversity>().Where(u => u.UniversityOfferID == uniOfferId).ToList();
+        }
+        public IEnumerable<ApplictionsToUniversity> GetStudentsApplicationToUniversity(int studentId)
+        {
+            return context.Set<ApplictionsToUniversity>().Where(u =>u.StudentId==studentId).ToList();
+        }
+    }
+}
